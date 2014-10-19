01f1a7a0721fde1848d1efc006b40be3f52f2487
Seviciu Cosmin Ioan <Only_cosmyn@yahoo.com>
2014-10-19 12:50:37 +0300

    navgation color changes

--- zet-tools/view/nav.php
+++ zet-tools/view/nav.php
@@ -4,9 +4,9 @@
 		<span class="zet-title"><a href="index.php">Zet-Tools</a></span>
 		<nav class = "pimp-nav led-slide">
 			<ul>
-				<li data-ledcolor = "#262626"><a href="index.php">Home</a></li>
-				<li data-ledcolor = "#262626"><a href="index.php?page=z-input">Z-Input</a></li>
-				<li class = "led-active_" data-ledcolor = "#262626"><a href="index.php?page=z-input-random">Random</a></li>
+				<li data-ledcolor = "#4e9fd9"><a href="index.php">Home</a></li>
+				<li data-ledcolor = "#3ed420"><a href="index.php?page=z-input">Z-Input</a></li>
+				<li class = "led-active_" data-ledcolor = "#ff0303"><a href="index.php?page=z-input-random">Random</a></li>
 				<!-- <li data-ledcolor = "#262626"><a href="">Contact</a></li> -->
 			</ul>
 		</nav>
